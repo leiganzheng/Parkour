6036d503286aa220bd0ed8ab9160893e913cee25
leiganzheng <leiganzheng@163.com>
2015-06-09 23:12:26 +0800

    XIUGUB

--- paoku/src/AnimationLayer.js
+++ paoku/src/AnimationLayer.js
@@ -2,23 +2,53 @@
  * Created by qoocc03 on 15/6/9.
  */
 var AnimationLayer = cc.Layer.extend({
-    ctor:function(){
+    spriteSheet:null,
+    runningAction:null,
+    sprite:null,
+    ctor:function(space){
         this._super()
+        this.space = space
         this.init()
+        this._debugNode = cc.PhysicsDebugNode.create(this.space);
+        // Parallax ratio and offset
+        this.addChild(this._debugNode, 10);
     },
     init:function(){
         this._super()
-        var winSize = cc.director.getWinSize()
-        var runner = cc.Sprite.create(res.runner_png)
-        runner.attr(
-            {
-                x: 80,
-                y: 85
-            }
-        )
-        var actionTo = cc.MoveTo.create(2,cc.p(300,85))
-        runner.runAction(actionTo)
-        this.addChild(runner)
+        // create sprite sheet
+        cc.spriteFrameCache.addSpriteFrame()
+        this.spriteSheet = cc.SpriteBatchNode.create(res.runner_png);
+        this.addChild(this.spriteSheet);
+        //init runningAction
+        var animFrames = [];
+        for (var i = 0;i<8;i++){
+            var str = "runner" + i + ".png";
+            var frame = cc.spriteFrameCache.getSpriteFrame(str);
+            animFrames.push(frame);
+        }
+        var animation = cc.Animation.create(animFrames, 0.1);
+        this.runningAction = cc.RepeatForever(cc.Animate(animation));
 
-    }
+        //1. create PhysicsSprite with a sprite frame name
+        this.sprite = cc.PhysicsSprite.create("#runner0.png");
+        var contentSize = this.sprite.getContentSize();
+        // 2. init the runner physic body
+        this.body = new cp.Body(1, cp.momentForBox(1, contentSize.width, contentSize.height));
+        //3. set the position of the runner
+        this.body.p = cc.p(g_runnerStartX, g_groundHight + contentSize.height / 2);
+        //4. apply impulse to the body
+        this.body.applyImpulse(cp.v(150, 0), cp.v(0, 0));//run speed
+        //5. add the created body to space
+        this.space.addBody(this.body);
+        //6. create the shape for the body
+        this.shape = new cp.BoxShape(this.body, contentSize.width - 14, contentSize.height);
+        //7. add shape to space
+        this.space.addShape(this.shape);
+        //8. set body to the physic sprite
+        this.sprite.setBody(this.body);
+        this.addChild(this.sprite)
+    },
+    getEyeX:function () {
+        return this.sprite.getPositionX() - g_runnerStartX;
+    },
 });
--- paoku/src/PlayScene.js
+++ paoku/src/PlayScene.js
@@ -1,12 +1,43 @@
 /**
  * Created by qoocc03 on 15/6/9.
  */
+
 var PlayScene = cc.Scene.extend({
+    space: null,
     onEnter:function () {
         this._super();
-        //add three layer in the right order
-        this.addChild(new BackgroundLayer());
-        this.addChild(new AnimationLayer());
-        this.addChild(new StatusLayer());
+        this.initPhysics();
+        this.gameLayer = cc.Layer.create();
+
+        //add Background layer and Animation layer to gameLayer
+        this.gameLayer.addChild(new BackgroundLayer(), 0, TagOfLayer.background);
+        this.gameLayer.addChild(new AnimationLayer(this.space), 0, TagOfLayer.Animation);
+        this.addChild(this.gameLayer);
+        this.addChild(new StatusLayer(), 0, TagOfLayer.Status);
+
+        this.scheduleUpdate();
+    },
+    // init space of chipmunk
+    initPhysics:function() {
+        //1. new space object
+        this.space = new cp.Space();
+        //2. setup the  Gravity
+        this.space.gravity = cp.v(0, -350);
+
+        // 3. set up Walls
+        var wallBottom = new cp.SegmentShape(this.space.staticBody,
+            cp.v(0, g_groundHight),// start point
+            cp.v(4294967295, g_groundHight),// MAX INT:4294967295
+            0);// thickness of wall
+        this.space.addStaticShape(wallBottom);
+    },
+    update:function (dt) {
+        // chipmunk step
+        this.space.step(dt);
+
+        var animationLayer = this.gameLayer.getChildByTag(TagOfLayer.Animation);
+        var eyeX = animationLayer.getEyeX();
+
+        this.gameLayer.setPosition(cc.p(-eyeX,0));
     }
 });
